b378f72deed0f0ec49ff79a17c3ede8e4e93c4ad
Fardeel Azhar <fardeelazhar82@gmail.com>
2025-06-01 17:59:59 +0500

    hitl fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,9 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("com.google.devtools.ksp")
+    id("com.google.dagger.hilt.android")
+
 }
 
 android {
@@ -57,4 +60,8 @@ dependencies {
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
     implementation(libs.androidx.core.splashscreen)
+
+    //hiltAdd commentMore actions
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.android.compiler)
 }
