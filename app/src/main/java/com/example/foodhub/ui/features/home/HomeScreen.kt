7375978f958cb605c96e9a765ae7057aefbcf3aa
Fardeel Azhar <fardeelazhar82@gmail.com>
2025-10-18 18:37:44 +0500

    feat: login flow with viewmodel implemented.

---
+++ app/src/main/java/com/example/foodhub/ui/features/home/HomeScreen.kt
@@ -0,0 +1,18 @@
+package com.example.foodhub.ui.features.home
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+
+@Composable
+fun HomeScreen(navController: NavController) {
+    Box(Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+
+        Text("Home Screen")
+
+    }
+}
